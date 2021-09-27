29a57f44125e9736eebb1c11f0c3b731724dd875
zeromook <zero-mook@naver.com>
2021-09-27 14:06:08 +0900

    second commit

---
+++ chap14/src/chap14/Pretest.java
@@ -0,0 +1,43 @@
+package chap14;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Pretest {
+
+	public static void main(String[] args) {
+		// 드라이버 검색하기
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("드라이버 검색 성공!");
+		}
+		catch(ClassNotFoundException e) {
+			System.err.println("드라이버 검색 실패!");
+			System.exit(0);
+		}
+		
+		// 드라이버 연결하기
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:XE","hr","hr");
+			System.out.println("데이터베이스 연결 성공!");
+		}
+		catch(SQLException e) {
+			System.out.println("데이터베이스 연결 실패!");
+			System.exit(0);
+		}
+		
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
